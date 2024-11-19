c1a0cfc5906021bf09066fe6fcb21139eace11e4
Raka Suryaardi Widjaja <raka.suryaardi@gmail.com>
2024-11-19 21:15:47 +0700

    add book transformer script

---
+++ src/scripts/BookTransformer.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import book from "../assets/data/mengzi.json" assert { type: "json" }
+import fs from "fs"
+
+function main() {
+  const result = {
+    chapters: {}
+  }
+  for (const chapter of book.chapters) {
+    const sections = {}
+    for (const section of chapter.sections) {
+      sections[section.slug] = {
+        title: section.title,
+        description: section.description,
+        content: section.content,
+      }
+    }
+
+    result.chapters[chapter.slug] = {
+      title: chapter.title,
+      description: chapter.description,
+      content: chapter.content,
+      summary: chapter.summary,
+      sections
+    }
+  }
+
+  const resultText = JSON.stringify(result)
+  fs.writeFileSync(`./result.json`, resultText)
+}
+
+main()
