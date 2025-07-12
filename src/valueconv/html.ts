5d7ea052d60b2c0685f774d56279f6397ad106b2
Raka Suryaardi Widjaja <raka.suryaardi@gmail.com>
2025-07-12 21:44:09 +0700

    replace html value converter

--- src/valueconv/html.ts
+++ src/valueconv/html.ts
@@ -1,13 +1,9 @@
-const div = document.createElement("div")
+import { convert } from "html-to-text"
 
 export function toPlain(content?: string): string {
   if (!content) {
     return ""
   }
 
-  div.innerHTML = content
-    .replaceAll("<br />", " ")
-    .replaceAll("&nbsp;", " ")
-  
-  return div.textContent || div.innerText || ""
+  return convert(content)
 }
