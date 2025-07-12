9c1cdace7bbb6129022275b5b680884862cc8f2f
Raka Suryaardi Widjaja <raka.suryaardi@gmail.com>
2025-07-12 22:34:35 +0700

    avoid converting using html-to-text library when the user agent is react-snap

--- src/valueconv/html.ts
+++ src/valueconv/html.ts
@@ -5,5 +5,12 @@ export function toPlain(content?: string): string {
     return ""
   }
 
+  // @note: cannot do the prerender if we're using the `convert` function
+  // that's why we're just `returning` the content as it's
+  const isReactSnap = typeof window !== "undefined" && window.navigator && window.navigator.userAgent === "ReactSnap"
+  if (isReactSnap) {
+    return content
+  }
+
   return convert(content)
 }
